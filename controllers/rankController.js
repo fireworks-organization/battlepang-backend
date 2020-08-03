c1fdf8630fa7f331fbf22e37e91b4f095570ec29
sungkuk5420 <sungkuk5420@gmail.com>
2020-08-03 15:09:28 +0900

    - 랭크 취득 API에 배틀, 조회수, 골드에따라 취득가능하도록 옵션 추가

--- controllers/rankController.js
+++ controllers/rankController.js
@@ -2,22 +2,32 @@ import passport from "passport";
 import dotenv from "dotenv";
 import User from "../models/User";
 import Rank from "../models/Rank";
+import Battle from "../models/Battle";
 
 dotenv.config();
 
 export const ranks = async (req, res) => {
   const {
-    query: { rankBy },
+    query: { rankObject, rankDataForSearch, },
   } = req;
   try {
-    if (!rankBy) {
-      res.status(400).send({ error: "rankBy 파라미너를 확인해주세요. user, battle" });
+    if (!rankObject) {
+      res.status(400).send({ error: "rankObject 파라미너를 확인해주세요. user, battle" });
     }
-    const ranks = await User.aggregate([{ $sample: { size: 30 } }]);
-    const oneDayAgoRanks = await Rank.find({ dateKeywordForSearch: "one-day-ago" }).sort({ rank: -1 });
-    const oneWeekAgoRanks = await Rank.find({ dateKeywordForSearch: "one-week-ago" }).sort({ rank: -1 });
-    const oneMonthAgoRanks = await Rank.find({ dateKeywordForSearch: "one-month-ago" }).sort({ rank: -1 });;
-    res.status(200).send({ ranks, oneDayAgoRanks, oneWeekAgoRanks, oneMonthAgoRanks });
+    if (rankObject === "users") {
+      const ranks = await User.aggregate([{ $sample: { size: 30 } }]);
+      const beforeDateRanks = await Rank.find({ dateKeywordForSearch: rankDataForSearch }).sort({ rank: -1 });
+      res.status(200).send({ ranks, beforeDateRanks });
+    } else if (rankObject === "battles") {
+      if (rankDataForSearch == "gold") {
+        const ranks = await Battle.find().sort({ gold: -1 }).populate("creator").limit(30);
+        res.status(200).send({ ranks });
+      }
+      if (rankDataForSearch == "views") {
+        const ranks = await Battle.find().sort({ views: -1 }).populate("creator").limit(30);
+        res.status(200).send({ ranks });
+      }
+    }
   } catch (error) {
     console.log(error);
     res.status(400).send({ error });
