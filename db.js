b9864243b155cc675ee9d9b10f62099eb11b32e4
emforhs <arskrar@gmail.com>
2021-02-22 13:14:12 +0900

    db 연결 해제시 재연결하도록 추가

--- db.js
+++ db.js
@@ -2,15 +2,18 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(
-    process.env.MONGO_PROD_URL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    }
-);
+const m_connect = () => {
+    mongoose.connect(
+        process.env.MONGO_PROD_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        }
+    );
+}
 
+m_connect();
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅  Connected to DB");
@@ -18,3 +21,4 @@ const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError); 
+db.on('disconnect', m_connect) 
